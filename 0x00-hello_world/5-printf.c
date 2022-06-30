94784abe09b05a27cbb97da34dd156b00303f069
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-06-30 15:33:07 -0700

    c program

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 /**
 * main - prints the size of various types
@@ -6,6 +5,6 @@
 */
 int main(void)
 {
-puts(" \ "Programming is like building a multilingual puzzle");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
