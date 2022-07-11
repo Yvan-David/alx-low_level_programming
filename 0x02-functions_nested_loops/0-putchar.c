8e49e1fbcef83c3c16ab3caa25043a2eb77743fc
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-07-11 13:37:31 -0700

    nested

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints putchar
+ * Return: 0 otherwise, nonzero
+ */
+int main(void)
+{
+	_putchar ('_');
+	_putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
