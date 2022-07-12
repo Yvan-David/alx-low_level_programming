71e842bae8cf2f12eadbcbed2a3bd260942a9e09
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-07-12 14:45:37 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - prints sign conversions
+ * @n: is an argument
+ * Return: absolute number
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		return (n);
+	}
+}
