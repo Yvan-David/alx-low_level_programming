a35c8df1b4bfdca523db9b2a6a4cb541ec69f705
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-07-12 05:08:34 -0700

    functions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -> prints the 10 lowercase
+ */
+void print_alphabet_x10(void)
+
+{
+	int d;
+	int n;
+
+	for (n = 0; n <= 9; n++)
+
+	{
+		for (d = 'a'; d <= 'z'; d++)
+		{
+			_putchar(d);
+		}
+	}
+
+	_putchar('\n');
+}
