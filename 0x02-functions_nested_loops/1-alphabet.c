e663b3f96a249f41e47ff71cb914380b4af0721a
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-07-12 04:53:38 -0700

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,16 @@
 #include "main.h"
 /**
- * main -prints lowercase
- * Return: 0 otherwise, nonzero
+ * print_alphabet -> prints the lowercase
  */
 void print_alphabet(void)
+
 {
 	int d;
-	for(d = 'a'; d <= 'z'; d++)
+	for (d = 'a'; d <= 'z'; d++)
+
 	{
 		_putchar(d);
 	}
+
 	_putchar('\n');
 }
