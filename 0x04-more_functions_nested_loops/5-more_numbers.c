1b2f8e094226e42a341480c54f79b2b2606217b0
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-07-14 15:53:35 -0700

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints digits
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y < 15; y++)
+		{
+			_putchar(y + '0');
+		}
+		_putchar('\n');
+	}
+}
