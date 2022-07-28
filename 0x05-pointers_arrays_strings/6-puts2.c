9f923885f82d29784ae86076427beff7f1c49591
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-07-28 10:58:00 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - put fx
+ * @str: argument
+ */
+void puts2(char *str)
+{
+	int len, i;
+	
+	len = strlen(str);
+	for (i = 0; i < len; i += 2)
+		_putchar(str[i]);
+	_putchar('\n');
+}
