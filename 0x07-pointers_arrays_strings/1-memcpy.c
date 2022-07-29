50fa510e35b14d0c3c0a1360e655598f2ed095bf
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-07-29 10:47:07 -0700

    pointersx

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * @dest: destination
+ * @src: source
+ * @n: number
+ * Return: string
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		dest[a] = src[a];
+	return (dest);
+}
