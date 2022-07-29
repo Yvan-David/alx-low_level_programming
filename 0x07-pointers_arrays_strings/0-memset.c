cdbdff64e36f14c037d93de318f0862dfe14d06f
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-07-29 10:34:25 -0700

    pointersx

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,10 +5,12 @@
  * @s: arguement
  * @b: charact
  * @n: digit
+ * Return: a string
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
+
 	for (a = 0; a < n; a++)
 		s[a] = b;
 
