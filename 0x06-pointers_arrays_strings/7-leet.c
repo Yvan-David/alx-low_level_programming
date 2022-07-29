426a122455a57b1a4e2349f15742c3ffd194f18a
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-07-29 09:42:50 -0700

    pointers

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - function
+ * @x: argument
+ * Return: string
+ */
+char *leet(char *x)
+{
+	int a = 0; b; l = 5;
+	char tr[5] = {'A', 'E', 'O', 'T', 'L'};
+	char tn[5] = {'4', '3', '0', '7', '1'};
+
+	while (x[a])
+	{
+		b = 0;
+		while (b < l)
+		{
+			if (x[a] == trb[b] || x[a] - 32 == tr[b])
+				x[a] = tn[b];
+			b++;
+		}
+		a++;
+	}
+	return (x);
+}
