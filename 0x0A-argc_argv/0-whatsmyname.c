787705e2968102fc37ccc003aff90ed9397485c5
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-08-01 11:06:53 -0700

    argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - io function
+ * @argc: parameter
+ * @argv: array
+ * Return: 0 always
+ */
+int main(int argc__attribut__((unused)), char argv*[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
