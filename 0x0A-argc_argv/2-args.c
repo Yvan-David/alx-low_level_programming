260d6ef3a0325aee2aacb7d462d5ef3fed5c3d99
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-08-01 11:31:07 -0700

    argc_argv

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - io function
+ * @argc: parameter
+ * @argv: array
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argc[i]);
+	}
+	return (0);
+}
