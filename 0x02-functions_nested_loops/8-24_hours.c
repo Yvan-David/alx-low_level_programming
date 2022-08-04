b636ebe27b7812c990727a0b0b60a2a1ae0e1c42
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-08-04 09:18:18 -0700

    bouer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,1 +1,30 @@
-hhhhh
+#include "main.h"
+
+void jack_bauer(void)
+{
+	int ht = 0;
+	int h = 0;
+	int mt = 0;
+	int m = 0;
+
+	for (ht = 0; ht <= 2; ht++)
+	{
+		for (mt = 0; mt <= 5; mt++)
+		{
+			for (m = 0; m <= 9; m++)
+			{
+				if (ht == 2 && h == 4 || ht == 2 && h == 5 ||  ht == 2 && h == 6 || ht == 2 && h == 7 || ht == 2 && h == 8 || ht == 2 && h == 9)
+				{
+					break;
+				}
+				_putchar(ht);
+				_putchar(h);
+				_putchar(':');
+				_putchar(mt);
+				_putchar(m);
+				_putchar('\n');
+
+			}
+		}
+	}
+}
