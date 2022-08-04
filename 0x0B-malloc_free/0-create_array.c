c1a09bf60883a974957cdbc18b9f79a7b8e4c64b
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-08-04 12:11:01 -0700

    arrays

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * create_array - functions
+ * @size: huge number
+ * @c: argument
+ * Return: a pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *s;
+
+	if (size == 0)
+		return (NULL);
+	s = (char *)malloc(size * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		s[i] = c;
+	return (s);
+}
