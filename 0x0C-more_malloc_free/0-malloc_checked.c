1e62d629466ee9389e2f0f8a30d4d896fccf0f05
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-08-08 03:57:46 -0700

    more malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,15 +2,17 @@
 #include <stdlib.h>
 
 /**
- * malloc_cheked - cheking
+ * malloc_checked - cheking
  * @b: argument
+ * Return: argument
  */
 void *malloc_checked(unsigned int b)
 {
 	void *p;
+
 	p = malloc(b);
 
 	if (p == NULL)
 		exit(98);
-	return(p);
+	return (p);
 }
