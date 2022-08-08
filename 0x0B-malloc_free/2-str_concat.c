c2401a45cee49b49124f974bbc9d1e2fff7e3606
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-08-08 02:39:38 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,10 +19,12 @@ char *str_concat(char *s1, char *s2)
 		i++;
 	while (s2[j])
 		j++;
+
 	l = i + j;
 	s = (char *)malloc(l * sizeof(char) + 1);
 	if (s == NULL)
 		return (NULL);
+	j = 0;
 	while (k < l)
 	{
 		if (k < i)
