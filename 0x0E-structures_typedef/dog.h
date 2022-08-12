dc7475947b1fd7682b4e7e9cbe218bacd5f46c84
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-08-12 08:10:36 -0700

    structures_tydef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef "DOG_H"
+#define "DOG_H"
+
+/**
+ * struct dog - structures
+ * @name: of dog
+ * @age: of dog
+ * @owner: of dog
+ */
+struct dog
+{
+	char name[20];
+	float age;
+	char owner[20];
+};
+
+#endif
+
