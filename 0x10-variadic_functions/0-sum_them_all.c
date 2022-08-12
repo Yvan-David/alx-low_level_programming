5c2d890a1ae507b2a39b4e949c638243e8e36a4d
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-08-12 09:35:52 -0700

    variadic_functions

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ *
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	int i, x, sum = 0;
+	va_list values;
+	va_start (values, n);
+
+	for (i = 0; i < n; i++)
+	{
+		x = va_arg(values, int);
+		sum += x;
+	}
+	va_end(values);
+	return (sum);
+}
+
