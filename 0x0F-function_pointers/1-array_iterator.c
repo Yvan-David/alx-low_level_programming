564fc339128bac30881491fe6aa0ef0422569f9b
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-08-15 10:03:17 -0700

    function_pointers

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+#include <stddef.h>
+
+/**
+ * array_iterator - prints hex and dec numbers
+ * @array: arrays
+ * @size: array size
+ * @action: pointer function
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (action != NULL)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+			action(*(array + i));
+	}
+}
