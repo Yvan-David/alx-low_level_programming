ec08561f60653d51af7599ec5763f3c0bfdbc871
Irankunda Yvan David <irankundayvan2020@gmail.com>
2022-08-22 07:50:27 -0700

    fix

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,11 +4,12 @@
  * @name: argue
  * @age: argue
  * @owner: argue
+ * @d: name
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	(d -> name) = name;
-	(d -> age) = age;
-	(d -> owner) = owner;
+	(d->name) = name;
+	(d->age) = age;
+	(d->owner) = owner;
 }
