1606ac4cc1206a8511ea429794157ddcec0faf3a
Yvan-David <irankundayvan2020@gmail.com>
2022-10-04 16:30:35 +0200

    doubly_linked_lists

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * dlistint_len - print dlist
+ * @h: pointer
+ * Return: size dlist
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+int c = 0;
+const dlistint_t *tmp = h;
+if (h == NULL)
+{
+return (0);
+}
+while (tmp->next != NULL)
+{
+tmp = tmp->next;
+c++;
+}
+return (c);
+}
