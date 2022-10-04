1ad1d6674f2f6091b3e72c55b9ff4bd122c7259e
Yvan-David <irankundayvan2020@gmail.com>
2022-10-04 11:31:10 +0200

    doubly_linked_lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
+int c = 0;
+const dlistint_t *tmp = h;
 if (h == NULL)
 {
 return (0);
 }
-int c = 0;
-const dlistint_t *tmp = h;
 while (tmp->next != NULL)
 {
 printf("%d\n", tmp->n);
