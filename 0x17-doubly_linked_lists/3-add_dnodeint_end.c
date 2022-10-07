edf579e10e1ae0092ed636fcefe452748294fb12
Yvan-David <irankundayvan2020@gmail.com>
2022-10-07 13:50:53 +0200

    doubly_linked_lists

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * add_dnodeint - add node
+ * @n: variable
+ * @head: double pointer
+ * Return: size dlist
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+dlistint_t *new, *tmp = NULL;
+new = (malloc(sizeof(dlistint_t)));
+if (new == NULL)
+{
+printf("error");
+return (tmp);
+}
+if (*head == NULL)
+{
+new->n = n;
+new->prev = *head;
+*head = new;
+tmp = new;
+new->next = NULL;
+}
+new->n = n;
+tmp->next = new;
+new->prev = tmp;
+new->next = NULL;
+tmp = new;
+return (tmp);
+}
