b5defb365ea855c0df10a17ee1723aa5bfe400b7
Yvan-David <irankundayvan2020@gmail.com>
2022-10-07 17:51:59 +0200

    more_data_structures

--- 0x17-doubly_linked_lists/2-main.c
+++ 0x17-doubly_linked_lists/2-main.c
@@ -8,6 +8,56 @@
  *
  * Return: Always EXIT_SUCCESS.
  */
+ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+dlistint_t *new, *tmp = NULL;
+new = (malloc(sizeof(dlistint_t)));
+if (new == NULL)
+{
+printf("error");
+return (tmp);
+}
+if (*head == NULL)
+{
+new->n = n;
+new->prev = *head;
+*head = new;
+tmp = new;
+new->next = NULL;
+}
+new->n = n;
+//*head = new;
+//new->prev = *head; // neezx
+/*tmp->prev = new;
+new->next = tmp;
+new->prev = NULL;
+*head = new;
+tmp = new;
+free(new);*/
+new->n = n;
+(*head)->prev = new;
+new->next = NULL;
+new->prev = *head;
+*head = new;
+return (*head);
+}
+size_t print_dlistint(const dlistint_t *h)
+{
+int c = 0;
+const dlistint_t *tmp = h;
+if (h == NULL)
+{
+return (0);
+}
+while (tmp->next != NULL)
+{
+printf("%d\n", tmp->n);
+tmp = tmp->next;
+c++;
+}
+printf("%d\n", tmp->n);
+return (c + 1);
+}
 int main(void)
 {
     dlistint_t *head;
