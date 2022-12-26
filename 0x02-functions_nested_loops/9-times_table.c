513163de47dffb56a7d4b64e86bd84550a66d442
Yvan-David <irankundayvan2020@gmail.com>
2022-12-26 12:14:05 +0200

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,7 +5,7 @@
  */
 void times_table(void)
 {
-	int n = 0 , a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0, i = 0, j = 0;
+	int n = 0, a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0, i = 0, j = 0;
 
 	for (n = 0; n <= 9; n++)
 	{
@@ -13,8 +13,8 @@ void times_table(void)
 		_putchar(',');
 		if (b <= 9)
 		{
-			_putchar('.');
-			_putchar('.');
+			_putchar(' ');
+			_putchar(' ');
 			_putchar(b + '0');
 			_putchar(',');
 		}
@@ -27,111 +27,111 @@ void times_table(void)
 		}
 		if (c <= 9)
 		{
-			_putchar('.');
-			_putchar('.');
+			_putchar(' ');
+			_putchar(' ');
 			_putchar(c + '0');
 			_putchar(',');
 		}
 		else
 		{
-			_putchar('.');
+			_putchar(' ');
 			_putchar((c / 10) + '0');
 			_putchar((c % 10) + '0');
 			_putchar(',');
 		}
 		if (d <= 9)
 		{
-			_putchar('.');
-			_putchar('.');
+			_putchar(' ');
+			_putchar(' ');
 			_putchar(d + '0');
 			_putchar(',');
 		}
 		else
 		{
-			_putchar('.');
+			_putchar(' ');
 			_putchar((d / 10) + '0');
 			_putchar((d % 10) + '0');
 			_putchar(',');
 		}
 		if (e <= 9)
 		{
-			_putchar('.');
-			_putchar('.');
+			_putchar(' ');
+			_putchar(' ');
 			_putchar(e + '0');
 			_putchar(',');
 		}
 		else
 		{
-			_putchar('.');
+			_putchar(' ');
 			_putchar((e / 10) + '0');
 			_putchar((e % 10) + '0');
 			_putchar(',');
 		}
 		if (f <= 9)
 		{
-			_putchar('.');
-			_putchar('.');
+			_putchar(' ');
+			_putchar(' ');
 			_putchar(f + '0');
 			_putchar(',');
 		}
 		else
 		{
-			_putchar('.');
+			_putchar(' ');
 			_putchar((f / 10) + '0');
 			_putchar((f % 10) + '0');
 			_putchar(',');
 		}
 		if (g <= 9)
 		{
-			_putchar('.');
-			_putchar('.');
+			_putchar(' ');
+			_putchar(' ');
 			_putchar(g + '0');
 			_putchar(',');
 		}
 		else
 		{
-			_putchar('.');
+			_putchar(' ');
 			_putchar((g / 10) + '0');
 			_putchar((g % 10) + '0');
-			_putchar(',');
+			_putchar(' ');
 		}
 		if (h <= 9)
 		{
-			_putchar('.');
-			_putchar('.');
+			_putchar(' ');
+			_putchar(' ');
 			_putchar(h + '0');
 			_putchar(',');
 		}
 		else
 		{
-			_putchar('.');
+			_putchar(' ');
 			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
 			_putchar(',');
 		}
 		if (i <= 9)
 		{
-			_putchar('.');
-			_putchar('.');
+			_putchar(' ');
+			_putchar(' ');
 			_putchar(i + '0');
 			_putchar(',');
 		}
 		else
 		{
-			_putchar('.');
+			_putchar(' ');
 			_putchar((i / 10) + '0');
 			_putchar((i % 10) + '0');
 			_putchar(',');
 		}
 		if (j <= 9)
 		{
-			_putchar('.');
-			_putchar('.');
+			_putchar(' ');
+			_putchar(' ');
 			_putchar(j + '0');
 		}
 		else
 		{
-			_putchar('.');
+			_putchar(' ');
 			_putchar((j / 10) + '0');
 			_putchar((j % 10) + '0');
 		}
