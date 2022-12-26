2933ddbb4dc07ed92823796b60877d05045751ef
Yvan-David <irankundayvan2020@gmail.com>
2022-12-26 12:37:31 +0200

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,150 +1,31 @@
-#include "main.h"
-
 /**
- * times_table - prints times table
+ * times_table - print multiplication
  */
 void times_table(void)
 {
-	int n = 0, a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0, i = 0, j = 0;
+	int i, j, c;
 
-	for (n = 0; n <= 9; n++)
+	for (i = 0; i < 10; i++)
 	{
-		_putchar(a + '0');
-		_putchar(',');
-		if (b <= 9)
+		for (j = 0; j < 10; j++)
 		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(b + '0');
-			_putchar(',');
+			c = i * j;
+			if (c / 10 != 0)
+				_putchar('0' + (c / 10));
+
+			if (j != 9)
+			{
+				_putchar('0' + (c % 10));
+				_putchar(',');
+				_putchar(' ');
+				if (c < 10)
+					_putchar(' ');
+			}
+			else if (j == 9)
+			{
+				_putchar('0' + (c % 10));
+				_putchar('\n');
+			}
 		}
-		else
-		{
-			_putchar('.');
-			_putchar((b / 10) + '0');
-			_putchar((b % 10) + '0');
-			_putchar(',');
-		}
-		if (c <= 9)
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(c + '0');
-			_putchar(',');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar((c / 10) + '0');
-			_putchar((c % 10) + '0');
-			_putchar(',');
-		}
-		if (d <= 9)
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(d + '0');
-			_putchar(',');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar((d / 10) + '0');
-			_putchar((d % 10) + '0');
-			_putchar(',');
-		}
-		if (e <= 9)
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(e + '0');
-			_putchar(',');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar((e / 10) + '0');
-			_putchar((e % 10) + '0');
-			_putchar(',');
-		}
-		if (f <= 9)
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(f + '0');
-			_putchar(',');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar((f / 10) + '0');
-			_putchar((f % 10) + '0');
-			_putchar(',');
-		}
-		if (g <= 9)
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(g + '0');
-			_putchar(',');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar((g / 10) + '0');
-			_putchar((g % 10) + '0');
-			_putchar(',');
-		}
-		if (h <= 9)
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(h + '0');
-			_putchar(',');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
-			_putchar(',');
-		}
-		if (i <= 9)
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(i + '0');
-			_putchar(',');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
-			_putchar(',');
-		}
-		if (j <= 9)
-		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(j + '0');
-		}
-		else
-		{
-			_putchar(' ');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-		}
-		_putchar('\n');
-		a = a + 0;
-		b = b + 1;
-		c = c + 2;
-		d = d + 3;
-		e = e + 4;
-		f = f + 5;
-		g = g + 6;
-		h = h + 7;
-		i = i + 8;
-		j = j + 9;
 	}
 }
