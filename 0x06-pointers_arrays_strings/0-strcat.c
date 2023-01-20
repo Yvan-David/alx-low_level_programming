b7aa2a22e52ca7a8d47f990026576880ed03f671
Yvan-David <irankundayvan2020@gmail.com>
2023-01-20 10:34:22 +0100

    pointers and arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -3,13 +3,28 @@
 #include <stdlib.h>
 #include <string.h>
 /**
- * _strcat -> prints function
- * @dest: argument one
- * @src: argument two
- * Return: string
- */
+* _strcat -> prints function
+* @dest: argument one
+* @src: argument two
+* Return: string
+*/
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
-	return (dest);
+int i, j;
+for (i = 0; dest[i] != '\0'; i++)
+{
+
+}
+if (dest[i] == '\0')
+{
+j = i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[j] = src[i];
+j++;
+}
+if (src[i] == '\0')
+dest[j] = src[i];
+}
+return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <stdio.h>
 
 /**
  * _strncat - prints fx
@@ -10,6 +11,41 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n);
-	return (dest);
+int i, j;
+for (i = 0; dest[i] != '\0'; i++)
+{
+
+}
+if (dest[i] == '\0')
+{
+j = i;
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[j] = src[i];
+j++;
+if ((i + 1) == n)
+	break;
+}
+if (src[i] == '\0')
+dest[j] = src[i];
+}
+return (dest);
+}
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
 }
