0dea9417bbdf5aef7391a1cb852772e1ad8391d2
Yvan-David <irankundayvan2020@gmail.com>
2023-01-25 02:39:36 -0800

    unfinished tasks

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,47 +2,29 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _strspn - string char
- * @s: argument
- * @accept: char
- * Return: string
- */
- unsigned int _strspn(char *s, char *accept)
- {
-  int i, j;
-  unsigned int count = 0;
-  for (i = 0; *accept != '\0'; i++)
-  {
-    for (j = 0; *s != '\0'; j++)
-    {
-        if (*s == *accept)
-        {
-           count = count + 1;
-           break;
-        }
-        s++;
-    }
-    s = s - j;
-    accept++;
-  }
-  printf("%lu\n", strspn("hello, world", "dleh"));
-  return (count + 1);
- }
- #include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+* _strspn - string char
+* @s: argument
+* @accept: char
+* Return: string
+*/
+unsigned int _strspn(char *s, char *accept)
 {
-    char *s = "hello, world";
-    char *f = "oleh";
-    unsigned int n;
-
-    n = _strspn(s, f);
-    printf("%u\n", n);
-    return (0);
+int i, j;
+unsigned int count = 0;
+for (i = 0; *accept != '\0'; i++)
+{
+for (j = 0; *s != '\0'; j++)
+{
+if (*s == *accept)
+{
+count = count + 1;
+break;
+}
+s++;
+}
+s = s - j;
+accept++;
+}
+printf("%lu\n", strspn("hello, world", "dleh"));
+return (count + 1);
 }
--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,47 +2,28 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * _strspn - string char
- * @s: argument
- * @accept: char
- * Return: string
- */
- char *_strpbrk(char *s, char *accept)
- {
-  int i, j;
-  unsigned int count = 0;
-  for (i = 0; *s != '\0'; i++)
-  {
-    for (j = 0; *accept != '\0'; j++)
-    {
-        if (*s == *accept)
-        {
-           return (s);
-           break;
-        }
-        accept++;
-    }
-    accept = accept - j;
-    s++;
-  }
- // printf("%lu\n", strspn("hello, world", "dleh"));
-  return (NULL);
- }
- #include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
+* _strpbrk - string char
+* @s: argument
+* @accept: char
+* Return: string
+*/
+char *_strpbrk(char *s, char *accept)
 {
-    char *s = "hello, world";
-    char *f = "world";
-    char *t;
-
-    t = _strpbrk(s, f);
-    printf("%s\n", t);
-    return (0);
+int i, j;
+unsigned int count = 0;
+for (i = 0; *s != '\0'; i++)
+{
+for (j = 0; *accept != '\0'; j++)
+{
+if (*s == *accept)
+{
+return (s);
+break;
+}
+accept++;
+}
+accept = accept - j;
+s++;
+}
+return (NULL);
 }
